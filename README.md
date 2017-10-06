41b884f12c56a993ed628a9d8e2a0728ba601684
James Chartrand <jc.chartrand@gmail.com>
2017-10-06 12:01:50 -0400

    feat: adds all entity calls to VIAF

--- README.md
+++ README.md
@@ -17,7 +17,9 @@
 
 ### Overview
 
-Finds entities (people, places, organizations) in VIAF.  Meant to be used with [cwrc-public-entity-dialogs](https://github.com/cwrc-public-entity-dialogs)
+Finds entities (people, places, organizations, titles) in VIAF.  Meant to be used with [cwrc-public-entity-dialogs](https://github.com/cwrc-public-entity-dialogs) where it runs in the browser.
+
+Although it will not work in node.js as-is, it does use the [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API) for http requests, and so could likely therefore use a browser/node.js compatible fetch implementation like: [isomorphic-fetch](https://www.npmjs.com/package/isomorphic-fetch).
 
 ### Installation
 
@@ -33,10 +35,13 @@ let viafLookup = require('viaf-entity-lookup');
 
 ###### findPlace(query)
 
-###### findOrganization(query)
+###### findOrganization(query)  
+  
+###### findTitle(query)  
 
-
-where the 'query' argument is an object:
+<br><br>
+where the 'query' argument is an object:  
+<br>  
 
 ```
 {
@@ -45,17 +50,70 @@ where the 'query' argument is an object:
 }
 ```
 
-and all the methods return promises that resolve to the following object:
+<br>
+and all find* methods return promises that resolve to an object like the following:
+<br><br>  
 
 ```
 {
-    name: a string - the name of the entity to display,
-    uri: uri to be used as the Linked Data URI for the entity,
-    id: same as uri
+    id: "http://viaf.org/viaf/9447148209321300460003/"
+    
+    name: "Fay Jones School of Architecture and Design"
+    
+    nameType: "Corporate"
+    
+    originalQueryString: "jones"
+    
+    repository: "VIAF"
+    
+    uri: "http://viaf.org/viaf/9447148209321300460003/"
+    
 }
 ```
+<br><br>
+There are a further four methods that are mainly made available to facilitate testing (to make it easier to mock calls to the VIAF service):
+
+###### getPersonLookupURI(query)
+
+###### getPlaceLookupURI(query)
+
+###### getOrganizationLookupURI(query)  
+  
+###### getTitleLookupURI(query) 
+
+<br><br>
+where the 'query' argument is the entity name to find and the methods return the VIAF URL that in turn returns results for the query.
 
 ### Development
 
-See [CWRC-Writer-Dev-Docs](https://github.com/jchartrand/CWRC-Writer-Dev-Docs) for extensive information about developing with CWRC-Writer GitHub repositories, including this one.
+[CWRC-Writer-Dev-Docs](https://github.com/jchartrand/CWRC-Writer-Dev-Docs) describes general development practices for CWRC-Writer GitHub repositories, including this one.
+
+#### Testing
+
+The code in this repository is intended to run in the browser, and so we use [browser-run](https://github.com/juliangruber/browser-run) to run [browserified](http://browserify.org) [tape](https://github.com/substack/tape) tests directly in the browser. 
+
+We [decorate](https://en.wikipedia.org/wiki/Decorator_pattern) [tape](https://github.com/substack/tape) with [tape-promise](https://github.com/jprichardson/tape-promise) to allow testing with promises and async methods.  
+
+#### Mocking
+
+We use [fetch-mock](https://github.com/wheresrhys/fetch-mock) to mock http calls (which we make using the [Fetch API](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API) rather than XMLHttpRequest). 
+
+We use [sinon](http://sinonjs.org) [fake timers](http://sinonjs.org/releases/v4.0.1/fake-timers/) to test our timeouts, without having to wait for the timeouts.
+#### Code Coverage  
+
+We generate code coverage by instrumenting our code with [istanbul](https://github.com/gotwarlost/istanbul) before [browser-run](https://github.com/juliangruber/browser-run) runs the tests, 
+then extract the coverage (which [istanbul](https://github.com/gotwarlost/istanbul) writes to the global object, i.e., the window in the browser), format it with [istanbul](https://github.com/gotwarlost/istanbul), and finally report (Travis actually does this for us) to [codecov.io](codecov.io)
+
+#### Transpilation
+
+We use [babelify](https://github.com/babel/babelify) and [babel-plugin-istanbul](https://github.com/istanbuljs/babel-plugin-istanbul) to compile our code, tests, and code coverage with [babel](https://github.com/babel/babel)  
+
+#### Continuous Integration
+
+We use [Travis](https://travis-ci.org).
+
+#### Release
+
+We follow [SemVer](http://semver.org), which [Semantic Release](https://github.com/semantic-release/semantic-release) makes easy.  
+Semantic Release also writes our commit messages, sets the version number, publishes to NPM, and finally generates a changelog and a release (including a git tag) on GitHub.
 
--- src/index.js
+++ src/index.js
@@ -1,31 +1,106 @@
-let viafWrapper = require('viaf-wrapper')
+'use strict';
+
+/*
+     config is passed through to fetch, so could include things like:
+     {
+         method: 'get',
+         credentials: 'same-origin'
+    }
+*/
+function fetchWithTimeout(url, config = {}, timeout = 8000) {
+
+        return new Promise((resolve, reject) => {
+            // the reject on the promise in the timeout callback won't have any effect, *unless*
+            // the timeout is triggered before the fetch resolves, in which case the setTimeout rejects
+            // the whole outer Promise, and the promise from the fetch is dropped entirely.
+            setTimeout(() => reject(new Error('Call to VIAF timed out')), timeout);
+            fetch(url, config).then(resolve, reject);
+        }).then(
+            response=>{
+                // check for ok status
+                if (response.ok) {
+                    return response.json()
+                }
+                // if status not ok, through an error
+                throw new Error(`Something wrong with the call to VIAF, possibly a problem with the network or the server. HTTP error: ${response.status}`);
+            }/*,
+            // instead of handling and rethrowing the error here, we just let it bubble through
+            error => {
+            // we could instead handle a reject from either of the fetch or setTimeout promises,
+            // whichever first rejects, do some loggingor something, and then throw a new rejection.
+                console.log(error)
+                return Promise.reject(new Error(`some error jjk: ${error}`))
+            }*/
+        )
+}
+
+// note that this method is exposed on the npm module to simplify testing,
+// i.e., to allow intercepting the HTTP call during testing, using sinon or similar.
+function getEntitySourceURI(queryString, methodName) {
+    return `http://viaf.org/viaf/search?query=${methodName}+all+%22${encodeURIComponent(queryString)}%22&httpAccept=application/json&maximumRecords=5`;
+}
+
+function getPersonLookupURI(queryString) {
+    return getEntitySourceURI(queryString, 'local.personalNames')
+}
+
+function getPlaceLookupURI(queryString) {
+    return getEntitySourceURI(queryString, 'local.geographicNames')
+}
+
+function getOrganizationLookupURI(queryString) {
+    return getEntitySourceURI(queryString, 'local.corporateNames')
+}
+
+function getTitleLookupURI(queryString) {
+    return getEntitySourceURI(queryString, 'local.uniformTitleWorks')
+}
+
+async function callVIAF(url, queryString) {
+
+
+        let parsedJSON = await fetchWithTimeout(url)
+
+        return parsedJSON.searchRetrieveResponse.records ? parsedJSON.searchRetrieveResponse.records.map(
+            ({
+                 record: {
+                     recordData: {
+                         nameType,
+                         Document: {'@about': uri},
+                         mainHeadings: {data: {text: name}}
+                     }
+                 }
+             }) => {
+                return {nameType, id: uri, uri, name, repository: 'VIAF', originalQueryString: queryString}
+            }) : []
+
+
+}
 
 async function findPerson(queryString) {
-    let viafResults = await viafWrapper.searchPersonalNames(queryString)
-    let simplifiedResults = viafResults.map(result=>{
-        return {name: result.heading, uri: result.primaryTopic, birthDate: result.birthDate, deathDate: result.deathDate
-        }})
-    return {originalQueryString: queryString, results:  simplifiedResults }
+    return callVIAF(getPersonLookupURI(queryString), queryString)
 }
 
 async function findPlace(queryString) {
-    let viafResults = await viafWrapper.searchGeographic(queryString)
-    let simplifiedResults = viafResults.map(result=>{
-        return {name: result.heading, uri: result.primaryTopic}})
-    return {originalQueryString: queryString, results:  simplifiedResults }
+    return callVIAF(getPlaceLookupURI(queryString), queryString)
 }
 
 async function findOrganization(queryString) {
-
-    let simplifiedResults = (await viafWrapper.searchCorporate(queryString)).map(result=>{
-        return {name: result.heading, uri: result.primaryTopic}})
-    return {originalQueryString: queryString, results:  simplifiedResults }
+    return callVIAF(getOrganizationLookupURI(queryString), queryString)
 }
 
+async function findTitle(queryString) {
+    return callVIAF(getTitleLookupURI(queryString), queryString)
+}
 
 module.exports = {
     findPerson: findPerson,
     findPlace: findPlace,
     findOrganization: findOrganization,
-
+    findTitle: findTitle,
+    getPersonLookupURI: getPersonLookupURI,
+    getPlaceLookupURI: getPlaceLookupURI,
+    getOrganizationLookupURI: getOrganizationLookupURI,
+    getTitleLookupURI: getTitleLookupURI,
+    fetchWithTimeout: fetchWithTimeout
 }
